7380c2675344a1e90b57284791490c7c343a340e
ShlomoLove <snassau@gmail.com>
2019-01-22 12:30:44 -0800

    have a random swim rendered from server button click

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -3,8 +3,35 @@ $('body').on('keydown', (event) => {
   var arrowPress = event.key.match(/Arrow(Up|Down|Left|Right)/);
   if (arrowPress) {
     var direction = arrowPress[1];
-    SwimTeam.move(direction.toLowerCase());
+    // SwimTeam.move(direction.toLowerCase());
+    $.ajax({
+      url: 'http://127.0.0.1:3000',
+      type: 'POST',
+      error: (error) => {console.log('error', error)},
+      data: JSON.stringify(direction),
+      success: (data) => {console.log('success', data)},
+      complete: () => {console.log('complete')},
+    })
   }
 });
 
 console.log('Client is running in the browser!');
+
+
+
+$('.changeButton').on('click', function(event){
+  var moves = ['left', 'right', 'up', 'down'];
+  var randomNum = Math.floor(Math.random() * 4);
+  SwimTeam.move(moves[randomNum]);
+})
+
+$('.ajaxButton').on('click', function(event){
+  $.ajax({
+    url: 'http://127.0.0.1:3000',
+    type: 'GET',
+    error: (error) => {console.log('error', error)},
+    success: (data) => {SwimTeam.move(data)},
+    complete: () => {console.log('complete')},
+
+  })
+})
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,6 +1,31 @@
 const headers = require('./cors');
 
+var sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+}
+
+function randomMove () {
+  var moves = ['left', 'right', 'up', 'down'];
+  var randomNum = Math.floor(Math.random() * 4);
+  return moves[randomNum];
+}
+
 module.exports = (req, res) => {
-  res.writeHead(200, headers);
-  res.end();
+  if (req.method === 'GET') {
+    console.log ('request received');
+    sendResponse(res, randomMove(), 200);
+  }
+
+  if (req.method === 'POST') {
+    console.log ('post received');
+    sendResponse(res, 'hello', 201)
+
+
+  }
+  // res.writeHead(200, headers);
+  // res.end();
 };
+
+
